d422ac5df42204c923dfc905ac7042f184988bf7
Iqbal Hamdani <miqbalh94@gmail.com>
2022-03-26 23:26:43 +0700

    Express sequelize ejs dashboard, forum discussion 6

---
+++ chapter-6/express-game/create.js
@@ -0,0 +1,24 @@
+const { Game, Biodata, History } = require("./models");
+
+// Game.create({
+//   username: "Fulstack Web",
+//   password: "fsw",
+// });
+
+// Biodata.create({
+//   name: 'Gading',
+//   hobby: 'Reading',
+//   game_id: 2,
+// })
+
+// History.create({
+//   playedAt: Date.now(),
+//   score: 60,
+//   GameId: 1,
+// });
+
+// History.create({
+//   playedAt: Date.now(),
+//   score: 90,
+//   GameId: 1,
+// });
